3bf83c67cbf37a7fe2b3202218eb5ebb4566a2f6
prep0ster0us <rbabu1@unh.newhaven.edu>
2024-05-01 13:55:21 -0400

    Adjusted potd timer for same day refresh

--- MathMindset-Murphy-Babu/Home/View/HomeView.swift
+++ MathMindset-Murphy-Babu/Home/View/HomeView.swift
@@ -114,18 +114,11 @@ struct HomeView: View {
                         ForEach(titles, id: \.self) { title in
                             let problemSet = title == "Factoring" ? PolySet : (title == "Trig" ? TrigSet : DerivativeSet)
                             NavigationLink(destination:
-//                                            ProblemView(
-//                                                topic: title,
-//                                                problemNum: topicProgress[title] as! Int+1,
-//                                                question: problemSet[topicProgress[title] as! Int].question,
-//                                                choices: problemSet[topicProgress[title] as! Int].choices,
-//                                                problemSet: problemSet
-//                                            )
-                                           ProblemsView(
-                                               topic: title,
-                                               problemSet: problemSet,
-                                               problemNum: CGFloat(topicProgress[title] as! Int)
-                                           )
+                                            ProblemsView(
+                                                topic: title,
+                                                problemSet: problemSet,
+                                                problemNum: CGFloat(topicProgress[title] as! Int)
+                                            )
                             ) {
                                 TopicCard(name: title, image: title, completed: topicProgress[title] as! Int, quizScore: quizScores[title] as! Int)
                                     .frame(width: $app.screenWidth.wrappedValue)
@@ -144,20 +137,20 @@ struct HomeView: View {
     
     func fetchProblemSet(_ name: String) {
         let docName = (name == "Factoring") ? "Poly" : name
-//        print(docName)
+        //        print(docName)
         let problemSet = (docName == "Poly") ? PolySet : ((docName == "Trig") ? TrigSet : DerivativeSet)
         if !problemSet.isEmpty { return }
         
         db.collection("Problems").document(docName).getDocument { (document, error) in
             if let document = document, document.exists {
-//                isLoading = true
+                //                isLoading = true
                 let data: [String: Any] = document.data() ?? [:]
-
+                
                 for (probNum, problem ) in data {
                     let problemInfo = problem as! NSDictionary
                     let question = problemInfo["question"]!
                     let choices = problemInfo["choices"]!
-
+                    
                     let problemData = ProblemData(id: probNum,
                                                   question: question as? String ?? "",
                                                   choices: choices as? [String] ?? [])
@@ -193,8 +186,8 @@ struct HomeView: View {
             .document(Auth.auth().currentUser!.uid)
             .getDocument(as: UserData.self) { result in
                 switch result {
-                    case .success(let document):
-//                    let decodedData = try JSONDecoder().decode(UserData.self, from: document)
+                case .success(let document):
+                    //                    let decodedData = try JSONDecoder().decode(UserData.self, from: document)
                     topicProgress = document.progress as [String: Int]
                     userScore = document.score
                     userStreak = document.streak
@@ -202,31 +195,32 @@ struct HomeView: View {
                     
                     // check if problem of the day has been solved already
                     // last POTD solve is less than the POTD refresh timestamp (presently using 9AM everyday)
-//                    potdActive = false           // reset before checking
+                    //                    potdActive = false           // reset before checking
                     if document.potd_timestamp < potdRefreshTimestamp() {
                         potdActive = false        // potd page should be made available
                     }
                     
                     // set flag to indicate all necessary data has been loaded in
                     isLoading = false
-
-                    case .failure(let error):
-                        print("Error fetching document: \(error)")
+                    
+                case .failure(let error):
+                    print("Error fetching document: \(error)")
                 }
-        }
+            }
     }
     
     func potdRefreshTimestamp() -> Date {
         let calendar = Calendar.current
+        let refreshDay = calendar.component(.hour, from: .now) < 9 ? 0 : 1
         let components = DateComponents(year: calendar.component(.year, from: .now),
                                         month: calendar.component(.month, from: .now),
-                                        day: calendar.component(.day, from: .now)+1,  // current day
+                                        day: calendar.component(.day, from: .now)+refreshDay,  // current day
                                         hour: 9,    // 9AM
                                         minute: 0,
                                         second: 0)
         return calendar.date(from: components)!
     }
-
+    
     func formattedTime() -> String {
         let seconds = Int(countDown)
         let hours = seconds / 3600
@@ -234,46 +228,8 @@ struct HomeView: View {
         let remainingSeconds = seconds % 60
         return String(format: "%02d:%02d:%02d", hours, minutes, remainingSeconds)
     }
-
+    
 }
-
-//struct TimerView: View {
-//    
-//    @State private var countDown = TimeInterval()
-//    @State private var timerRunning = true
-//    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-//    
-//    var test = true
-//    
-//    var body: some View {
-////        VStack {
-////            $app.probOfDaySolved.wrappedValue
-//            
-//            // if problem of the day is solved, show timer
-//            Text(test ? formattedTime() : "Solve")     // difference available in seconds, format tthe value in HH:MM:SS
-//                .font(.title)
-//                .onReceive(timer) { _ in
-//                    if countDown > 0  && timerRunning {
-//                        countDown -= 1
-//                    } else {
-//                        // by updating a state variable when the timer runs out, we can update the button to be active (so the problem of the day is made available)
-//                        //                            timerRunning = false
-//                        timer.upstream.connect().cancel()     // relinquish thread process
-//                    }
-//                }
-//            //
-//            //                if !timerRunning {
-//            //                    Text("display text when timer up!")
-//            //                        .font(.largeTitle)
-//            //                        .padding()
-//            //                }
-//            .onAppear {
-//                calculateTimeDifference()
-//            }
-//    }
-//    
-//   
-//}
 
 #Preview {
     HomeView()
