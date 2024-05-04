e810a477b340dc5dd8ce4a26422de976fbbbb5ec
prep0ster0us <rbabu1@unh.newhaven.edu>
2024-05-04 01:32:50 -0400

    Added animated onboarding slides

--- MathMindset-Murphy-Babu/Splash/View/OnboardingView.swift
+++ MathMindset-Murphy-Babu/Splash/View/OnboardingView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct OnboardingView: View {
     
     @State private var activeTab: Int = 0       // for tab view
-    @State private var slideTitle: String = "Numbers are fun!"
+    @State private var slideTitle: String = "Practice concepts"
     
     private var dotAppearance = UIPageControl.appearance()
     
@@ -18,46 +18,12 @@ struct OnboardingView: View {
     var content: some View {
         NavigationStack {
             VStack {
-                HStack {
-                    if activeTab == 0 {
-                        Text("Hello Solvers!")
-                            .font(.title)
-//                            .font(.custom("TheSerifHandBlack", size: 24))
-//                            .fontWeight(.bold)
-//                            .kerning(2.0)       // character spacing
-                    }
-                    Spacer()
-                    NavigationLink (destination: SignInView().navigationBarBackButtonHidden(true)) {
-                        Text(activeTab == 2 ? "Get Started" : "Skip")
-                            .fontWeight(.bold)
-                            .kerning(1.3)
-                    }.simultaneousGesture(TapGesture().onEnded {
-                        UserDefaults.standard.set(true, forKey: "firstRun")
-                    })
-                }.padding()
-                    .foregroundStyle(Color.black)
-                    .frame(height: 40)
-                
-                Text(slideTitle)
-//                    .font(.title)
-//                    .font(.custom("TheSerifHandExtraBlack", size: 48))
-//                    .fontWeight(.bold)
-//                    .kerning(1.5)
-                    .font(.title)
-                    .padding(.top, 36)
-                
                 TabView(selection: $activeTab) {
-                    Image("onboarding1")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
+                    onboarding1
                         .tag(0)
-                    Image("onboarding2")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
+                    onboarding2
                         .tag(1)
-                    Image("onboarding3")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
+                    onboarding3
                         .tag(2)
                 }.tabViewStyle(.page)
                     .indexViewStyle(.page(backgroundDisplayMode: .interactive))
@@ -67,16 +33,127 @@ struct OnboardingView: View {
                     }
                     .onChange(of: activeTab) {
                         switch(activeTab) {
-                        case 0: slideTitle = "Numbers are fun!"
-                        case 1: slideTitle = "Practice Problems"
-                        case 2: slideTitle = "Compete on Leaderboards"
+                        case 0: slideTitle = "Practice concepts"
+                        case 1: slideTitle = "Solve Problems"
+                        case 2: slideTitle = "Compete on the Leaderboards"
                         default: slideTitle = "Impact that math-ers"
                         }
                     }
             }
+            .background(ignoresSafeAreaEdges: .all)
+            .ignoresSafeArea(.all)
         }
     }
+    
+    var onboarding1: some View {
+        VStack {
+            HStack {
+                Spacer()
+                NavigationLink (destination: SignInView().navigationBarBackButtonHidden(true)) {
+                    Text("Skip")
+                        .fontWeight(.heavy)
+                        .foregroundStyle(.black)
+                        .padding(.trailing, 8)
+                }.simultaneousGesture(TapGesture().onEnded {
+                    UserDefaults.standard.set(true, forKey: "firstRun")
+                })
+            }.padding()
+                .frame(height: 40)
+//                .padding(.bottom, 12)
+            // main content
+            ZStack (alignment: .top) {
+                GIFLoader(gifName: "solveGIF")
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.top, 24)
+                    .offset(x: 12, y: 32)
+                Text(slideTitle)
+                    .font(.system(size: 36, weight: .heavy))
+            }
+            Image("onboardingSolve")
+                .resizable()
+                .frame(width: AppVariables().screenWidth-80, height: 350)
+                .padding(.bottom, 24)
+            Spacer()
+        }
+        .background(
+            .white
+        )
+    }
+    
+    var onboarding2: some View {
+        VStack {
+            HStack {
+                Spacer()
+                NavigationLink (destination: SignInView().navigationBarBackButtonHidden(true)) {
+                    Text("Skip")
+                        .fontWeight(.heavy)
+                        .foregroundStyle(.black)
+                        .padding(.trailing, 8)
+                }.simultaneousGesture(TapGesture().onEnded {
+                    UserDefaults.standard.set(true, forKey: "firstRun")
+                })
+            }.padding()
+                .frame(height: 40)
+                .padding(.bottom, 12)
+            // main content
+            ZStack (alignment: .top) {
+                GIFLoader(gifName: "practiceGIF")
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.top, 24)
+                    .offset(y: 12)
+                Text(slideTitle)
+                    .font(.system(size: 36, weight: .heavy))
+            }
+            Image("onboardingPractice")
+                .resizable()
+                .frame(width: AppVariables().screenWidth-80)
+                .padding(.bottom, 24)
+            Spacer()
+        }
+        .background(
+            .white
+        )
+    }
+    
+    var onboarding3: some View {
+        VStack {
+            HStack {
+                Spacer()
+                NavigationLink (destination: SignInView().navigationBarBackButtonHidden(true)) {
+                    Text("Get Started")
+                        .fontWeight(.heavy)
+                        .foregroundStyle(.black)
+                        .padding(.trailing, 8)
+                }.simultaneousGesture(TapGesture().onEnded {
+                    UserDefaults.standard.set(true, forKey: "firstRun")
+                })
+            }.padding()
+                .frame(height: 40)
+                .padding(.bottom, 12)
+            // main content
+            ZStack (alignment: .top) {
+                GIFLoader(gifName: "leaderboardGIF")
+                    .frame(height: 285)
+                    .scaleEffect(1.6)
+                    .clipped()
+                    .padding(.top, 38)
+                    .offset(y: 32)
+                Text(slideTitle)
+                    .font(.system(size: 36, weight: .heavy))
+            }
+            Image("onboardingLeaderboard")
+                .resizable()
+                .frame(width: AppVariables().screenWidth-80, height: 350)
+                .padding(.bottom, 24)
+                .offset(y: -12)
+            Spacer()
+        }
+        .background(
+            .onboardingLeaderboardbg
+        )
+    }
 }
+
 
 #Preview {
     OnboardingView()
